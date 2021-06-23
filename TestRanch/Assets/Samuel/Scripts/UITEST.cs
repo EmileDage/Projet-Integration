a6ef3a8a81c71c3ff30c9f10e0216577c1ad7066
Samuel Narbonne <Super_moi_10@hotmail.com>
2021-06-23 12:21:31 -0400

    PuzzleDoor Update

--- TestRanch/Assets/Samuel/Scripts/UITEST.cs
+++ TestRanch/Assets/Samuel/Scripts/UITEST.cs
@@ -8,6 +8,7 @@ public class UITEST : MonoBehaviour
     public Image staminaBar = null;
     public Image healthBar = null;
     public Text txt_ChronoCoin = null;
+    public Text txt_Keys = null;
     
 
     public Transform player = null;
@@ -25,6 +26,8 @@ public class UITEST : MonoBehaviour
         staminaBar.fillAmount = staminaModule.GetStamina().GetCurrentValue() / staminaModule.GetStamina().Value();
         healthBar.fillAmount = healthModule.GetHealth().GetCurrentValue() / healthModule.GetHealth().Value();
         txt_ChronoCoin.text = GameManager.gmInstance.GetChronoCoin() + "Cc";
+        txt_Keys.text = GameManager.gmInstance.GetPuzzleKey() + " Keys";
+
     }
 
 }
