348aa6e37d4ed6b0d0cc896accff57fd7872bd1b
CryingAquarium <courtneybluewater@hotmail.com>
2021-07-15 11:52:58 -0400

    WIP fusee end

--- TestRanch/Assets/NPC/END_Fusee.cs
+++ TestRanch/Assets/NPC/END_Fusee.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class END_Fusee : MonoBehaviour
+
+public class END_Fusee : MonoBehaviour, IInteractible
 {
-    // Start is called before the first frame update
-    void Start()
+    //idealement tu voit la fusee monter dans lespcae puis credits
+    [SerializeField] private GameObject credits;
+    [SerializeField] private GameObject pannel;
+    [SerializeField] private ParticleSystem engine_fire;
+    private Animator anime;
+    private GameManager gm;
+    private Player joueur;
+    private Camera cam_joueur;
+    [SerializeField] private Camera cam_fusee;
+
+
+    public void Interact(Player joueur)
     {
+
+        joueur.gameObject.SetActive(false);
+        cam_joueur.enabled = false;
+        cam_fusee.enabled = true;
+        anime.SetBool("GoingToSpace", true);
+        engine_fire.gameObject.SetActive(true);
+
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void RollCredits() {
+        pannel.SetActive(true);
+        credits.SetActive(true);
+
     }
+
+    private void Awake()
+    {
+        cam_fusee.enabled = false;//just to be safe to not fuck up normal cam
+
+    }
+
+    private void Start()
+    {
+        credits.SetActive(false);
+        pannel.SetActive(false);
+        //pannel.GetComponent<Ima>
+        engine_fire.gameObject.SetActive(false);
+        anime = this.gameObject.GetComponent<Animator>();
+        gm = GameManager.gmInstance;
+        joueur = gm.Joueur;
+        cam_joueur = gm.Joueur.playerCam.GetComponent<Camera>();
+    }
+
+
 }
